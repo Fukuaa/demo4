8b78523d1c3b60b00db53573ea914c6b5dcbf9ba
zqf <1936534366@qq.com>
2021-10-31 19:32:42 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/rcontroller.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class rcontroller {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "views/login";
+    }
+
+    @RequestMapping("/l1/{id}")
+    public String l1(@PathVariable("id") int id){
+        return "views/l1/"+id;
+    }
+    @RequestMapping("/l2/{id}")
+    public String l2(@PathVariable("id") int id){
+        return "views/l2/"+id;
+    }
+    @RequestMapping("/l3/{id}")
+    public String l3(@PathVariable("id") int id){
+        return "views/l3/"+id;
+    }
+}
