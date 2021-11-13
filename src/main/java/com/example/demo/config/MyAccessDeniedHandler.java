9acc9e2e5c9e5f3164f93c293113252829496459
zqf <1936534366@qq.com>
2021-11-13 14:22:25 +0800

    无权限返回字符串

---
+++ src/main/java/com/example/demo/config/MyAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        //返回json形式的错误信息
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.getWriter().println("你没有权限，请充钱");
+        httpServletResponse.getWriter().flush();
+    }
+}
